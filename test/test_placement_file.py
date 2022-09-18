b253db62b0eb3d39598aa5d22edef171103e2af6
NULL <40719961+yannl35133@users.noreply.github.com>
2022-09-19 01:54:34 +0200

    Fix test

--- test/test_placement_file.py
+++ test/test_placement_file.py
@@ -5,7 +5,7 @@ from graph_logic.logic_input import Areas
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from ssrando import Randomizer
+from ssrando import Hints, Randomizer
 from options import Options
 from graph_logic.placement_file import PlacementFile
 from yaml_files import graph_requirements, checks, hints, map_exits
@@ -20,6 +20,10 @@ def test_roundtrip():
         opts.set_option("seed", i)
         rando = Randomizer(areas, opts)
         rando.rando.randomize(useroutput)
+        rando.logic = rando.rando.extract_hint_logic()
+        del rando.rando
+        rando.logic.check(useroutput)
+        rando.hints = Hints(rando.options, rando.rng, rando.areas, rando.logic)
         rando.hints.do_hints()
         plcmt_file = rando.get_placement_file()
         round_tripped_file = PlacementFile()
