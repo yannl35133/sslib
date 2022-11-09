ssrando/ssrando

Chest dowsing options, start with treasure dowsing

gui/ui_randogui.py
@@ -215,7 +215,7 @@ class Ui_MainWindow(object):
         self.groupBox_17.setGeometry(QRect(10, 270, 331, 101))
         self.verticalLayoutWidget_16 = QWidget(self.groupBox_17)
         self.verticalLayoutWidget_16.setObjectName(u"verticalLayoutWidget_16")
-        self.verticalLayoutWidget_16.setGeometry(QRect(10, 20, 311, 75))
+        self.verticalLayoutWidget_16.setGeometry(QRect(10, 20, 311, 78))
         self.verticalLayout_39 = QVBoxLayout(self.verticalLayoutWidget_16)
         self.verticalLayout_39.setObjectName(u"verticalLayout_39")
         self.verticalLayout_39.setContentsMargins(0, 0, 0, 0)
@@ -922,6 +922,39 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_31.addItem(self.verticalSpacer_6)
 
+        self.groupBox_18 = QGroupBox(self.tab_4)
+        self.groupBox_18.setObjectName(u"groupBox_18")
+        self.groupBox_18.setGeometry(QRect(420, 270, 191, 241))
+        self.verticalLayoutWidget_17 = QWidget(self.groupBox_18)
+        self.verticalLayoutWidget_17.setObjectName(u"verticalLayoutWidget_17")
+        self.verticalLayoutWidget_17.setGeometry(QRect(10, 20, 171, 211))
+        self.verticalLayout_40 = QVBoxLayout(self.verticalLayoutWidget_17)
+        self.verticalLayout_40.setObjectName(u"verticalLayout_40")
+        self.verticalLayout_40.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_41 = QVBoxLayout()
+        self.verticalLayout_41.setObjectName(u"verticalLayout_41")
+
+        self.verticalLayout_40.addLayout(self.verticalLayout_41)
+
+        self.verticalLayout_42 = QVBoxLayout()
+        self.verticalLayout_42.setObjectName(u"verticalLayout_42")
+        self.label_for_option_chest_dowsing = QLabel(self.verticalLayoutWidget_17)
+        self.label_for_option_chest_dowsing.setObjectName(u"label_for_option_chest_dowsing")
+
+        self.verticalLayout_42.addWidget(self.label_for_option_chest_dowsing)
+
+        self.option_chest_dowsing = QComboBox(self.verticalLayoutWidget_17)
+        self.option_chest_dowsing.setObjectName(u"option_chest_dowsing")
+
+        self.verticalLayout_42.addWidget(self.option_chest_dowsing)
+
+
+        self.verticalLayout_40.addLayout(self.verticalLayout_42)
+
+        self.verticalSpacer_18 = QSpacerItem(20, 70, QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
+
+        self.verticalLayout_40.addItem(self.verticalSpacer_18)
+
         self.tabWidget.addTab(self.tab_4, "")
         self.tab_5 = QWidget()
         self.tab_5.setObjectName(u"tab_5")
@@ -1206,9 +1239,10 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(2)
         self.option_randomize_entrances.setCurrentIndex(-1)
         self.option_triforce_shuffle.setCurrentIndex(-1)
+        self.option_chest_dowsing.setCurrentIndex(-1)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -1327,6 +1361,9 @@ class Ui_MainWindow(object):
         self.option_triforce_required.setText(QCoreApplication.translate("MainWindow", u"Triforce Required", None))
         self.label_for_option_triforce_shuffle.setText(QCoreApplication.translate("MainWindow", u"Triforce Shuffle", None))
         self.option_triforce_shuffle.setCurrentText("")
+        self.groupBox_18.setTitle(QCoreApplication.translate("MainWindow", u"Dowsing Options", None))
+        self.label_for_option_chest_dowsing.setText(QCoreApplication.translate("MainWindow", u"Chest Dowsing", None))
+        self.option_chest_dowsing.setCurrentText("")
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("MainWindow", u"Additional Settings", None))
         self.label_for_option_logic_mode.setText(QCoreApplication.translate("MainWindow", u"Logic Mode", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Exclude Locations", None))

yannl35133/sslib

gui/ui_randogui.py
@@ -938,22 +938,37 @@ class Ui_MainWindow(object):
         self.verticalLayoutWidget_17 = QWidget(self.groupBox_18)
         self.verticalLayoutWidget_17.setObjectName(u"verticalLayoutWidget_17")
         self.verticalLayoutWidget_17.setGeometry(QRect(10, 20, 177, 211))
-        self.verticalLayout_40 = QVBoxLayout(self.verticalLayoutWidget_17)
-        self.verticalLayout_40.setObjectName(u"verticalLayout_40")
-        self.verticalLayout_40.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_41 = QVBoxLayout()
+        self.verticalLayout_41 = QVBoxLayout(self.verticalLayoutWidget_17)
         self.verticalLayout_41.setObjectName(u"verticalLayout_41")
+        self.verticalLayout_41.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_42 = QVBoxLayout()
+        self.verticalLayout_42.setObjectName(u"verticalLayout_42")
 
-        self.verticalLayout_40.addLayout(self.verticalLayout_41)
+        self.verticalLayout_41.addLayout(self.verticalLayout_42)
+
+        self.verticalLayout_43 = QVBoxLayout()
+        self.verticalLayout_43.setObjectName(u"verticalLayout_43")
+        self.label_for_option_chest_dowsing = QLabel(self.verticalLayoutWidget_17)
+        self.label_for_option_chest_dowsing.setObjectName(u"label_for_option_chest_dowsing")
+
+        self.verticalLayout_43.addWidget(self.label_for_option_chest_dowsing)
+
+        self.option_chest_dowsing = QComboBox(self.verticalLayoutWidget_17)
+        self.option_chest_dowsing.setObjectName(u"option_chest_dowsing")
+
+        self.verticalLayout_43.addWidget(self.option_chest_dowsing)
+
+
+        self.verticalLayout_41.addLayout(self.verticalLayout_43)
 
         self.option_fill_dowsing_on_white_sword = QCheckBox(self.verticalLayoutWidget_17)
         self.option_fill_dowsing_on_white_sword.setObjectName(u"option_fill_dowsing_on_white_sword")
 
-        self.verticalLayout_40.addWidget(self.option_fill_dowsing_on_white_sword)
+        self.verticalLayout_41.addWidget(self.option_fill_dowsing_on_white_sword)
 
         self.verticalSpacer_18 = QSpacerItem(20, 70, QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
 
-        self.verticalLayout_40.addItem(self.verticalSpacer_18)
+        self.verticalLayout_41.addItem(self.verticalSpacer_18)
 
         self.tabWidget.addTab(self.tab_4, "")
         self.tab_5 = QWidget()
@@ -1243,6 +1258,7 @@ class Ui_MainWindow(object):
         self.option_sword_dungeon_reward.setCurrentIndex(-1)
         self.option_randomize_entrances.setCurrentIndex(-1)
         self.option_triforce_shuffle.setCurrentIndex(-1)
+        self.option_chest_dowsing.setCurrentIndex(-1)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -1363,6 +1379,8 @@ class Ui_MainWindow(object):
         self.label_for_option_triforce_shuffle.setText(QCoreApplication.translate("MainWindow", u"Triforce Shuffle", None))
         self.option_triforce_shuffle.setCurrentText("")
         self.groupBox_18.setTitle(QCoreApplication.translate("MainWindow", u"Dowsing Options", None))
+        self.label_for_option_chest_dowsing.setText(QCoreApplication.translate("MainWindow", u"Chest Dowsing", None))
+        self.option_chest_dowsing.setCurrentText("")
         self.option_fill_dowsing_on_white_sword.setText(QCoreApplication.translate("MainWindow", u"Fill Dowsing on White Sword", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("MainWindow", u"Additional Settings", None))
         self.label_for_option_logic_mode.setText(QCoreApplication.translate("MainWindow", u"Logic Mode", None))